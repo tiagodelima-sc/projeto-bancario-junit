2103289a1ecc6c7b20daf9be905027370f293fbd
Tiago de Lima <tiago.l07@aluno.ifsc.edu.br>
2022-05-13 09:04:29 -0300

    Criando classes e metodos (parte 2)

---
+++ projeto-sistema-bancario/src/main/java/br/com/tiagodelima/Conta.java
@@ -0,0 +1,98 @@
+package br.com.tiagodelima;
+
+import java.util.List;
+
+/**
+ * Representa uma conta bancária de um determinado {@link Cliente}, tendo os
+ * seguintes requisitos:
+ */
+public class Conta implements Cadastro {
+
+    private long id;
+    private String numero;
+    private boolean poupanca;
+    private boolean especial;
+    private double saldo;
+    private double limite;
+    private List<Movimentacao> movimentacoes;
+
+    public Conta() {
+        // TODO: Você precisa implementar este método
+    }
+
+    public Conta(Agencia agencia, boolean especial, final double limite) {
+        // TODO: Você precisa implementar este método
+    }
+
+    public List<Movimentacao> getMovimentacoes() {
+        return movimentacoes;
+    }
+
+    public void addMovimentacao(Movimentacao movimentacao) {
+        // TODO: Você precisa implementar este método
+    }
+
+    public double getSaldoTotal() {
+        /* TODO: Você precisa implementar este método. 
+        A linha abaixo deve ser substituída pelo seu código */
+        return 0.0;
+    }
+
+    public void saque(final double valor) {
+        // TODO: Você precisa implementar este método
+    }
+
+    public void depositoDinheiro(final double valor) {
+        // TODO: Você precisa implementar este método
+    }
+
+    public void depositoCheque(final double valor) {
+        // TODO: Você precisa implementar este método
+    }
+
+    //@Override
+    public long getId() {
+        return id;
+    }
+
+    //@Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public boolean isPoupanca() {
+        return poupanca;
+    }
+
+    public void setPoupanca(boolean poupanca) {
+        this.poupanca = poupanca;
+    }
+
+    public boolean isEspecial() {
+        return especial;
+    }
+
+    public void setEspecial(boolean especial) {
+        this.especial = especial;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+}
---
+++ projeto-sistema-bancario/src/main/java/br/com/tiagodelima/Movimentacao.java
@@ -0,0 +1,69 @@
+package br.com.tiagodelima;
+
+import java.util.Objects;
+
+/**
+ * Representa uma movimentação em uma {@link Conta} bancária, que pode indicar entrada ou saída.
+ */
+public class Movimentacao implements Cadastro {
+   
+	private long id;
+    private String descricao;
+    private Conta conta;
+    private char tipo;
+    private double valor;
+    private boolean confirmada;
+
+    
+    public Movimentacao(Conta conta){
+        // TODO: Você precisa implementar este método
+    }
+
+    //@Override
+    public long getId() {
+        return id;
+    }
+
+   // @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public char getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(char tipo){
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Objects.requireNonNull(descricao, "Descrição não pode ser nula.");
+
+        if(descricao.trim().isEmpty()){
+            throw new IllegalArgumentException("Descrição não pode ser vazia.");
+        }
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean isConfirmada() {
+        return confirmada;
+    }
+
+    public void setConfirmada(boolean confirmada) {
+        this.confirmada = confirmada;
+    }
+
+}
